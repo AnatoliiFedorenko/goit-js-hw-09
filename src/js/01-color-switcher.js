87b6ad01afd5c9a5a057afb44758a11e9c7ae3a3
Anatolii Fedorenko <anatolyfedorenko@gmail.com>
2022-10-27 21:34:19 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+    startBtn.disabled = true;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  body.style.backgroundColor = '';
+});

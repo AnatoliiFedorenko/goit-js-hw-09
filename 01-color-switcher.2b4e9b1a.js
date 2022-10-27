9e2e26bf362c9fcc182862b780d28008c306eb82
AnatoliiFedorenko <103376512+AnatoliiFedorenko@users.noreply.github.com>
2022-10-27 18:34:43 +0000

    Deploying to gh-pages from @ AnatoliiFedorenko/goit-js-hw-09@87b6ad01afd5c9a5a057afb44758a11e9c7ae3a3 🚀

---
+++ 01-color-switcher.2b4e9b1a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=e,t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,n.style.backgroundColor=""}))}();
+//# sourceMappingURL=01-color-switcher.2b4e9b1a.js.map

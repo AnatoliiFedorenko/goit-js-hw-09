df011252f13b09c0a86b23a81383e79f805aa575
Anatolii Fedorenko <anatolyfedorenko@gmail.com>
2022-10-28 13:23:08 +0300

    Task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,100 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  clockfaceDay: document.querySelector('[data-days]'),
+  clockfaceHours: document.querySelector('[data-hours]'),
+  clockfaceMinutes: document.querySelector('[data-minutes]'),
+  clockfaceSeconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+let selectedTime = 0;
+let timerId = null;
+
+function settings() {
+  const currentDate = Date.now();
+
+  const deltaTime = selectedTime - currentDate;
+
+  const diffTime = convertMs(deltaTime);
+
+  updateClockface(diffTime);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.clockfaceDay.textContent = `${days}`;
+  refs.clockfaceHours.textContent = `${hours}`;
+  refs.clockfaceMinutes.textContent = `${minutes}`;
+  refs.clockfaceSeconds.textContent = `${seconds}`;
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedTime = selectedDates[0];
+    if (selectedDates[0] < Date.now()) {
+      alert('Please choose a date in the future');
+      refs.startBtn.disabled = true;
+      const diffTime = convertMs(0);
+      updateClockface(diffTime);
+      return;
+    } else refs.startBtn.disabled = false;
+    settings();
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+refs.startBtn.addEventListener('click', startTimer);
+
+function timer() {
+  settings();
+
+  const currentDate = Date.now();
+
+  const deltaTime = selectedTime - currentDate;
+
+  if (deltaTime <= 0) {
+    clearInterval(timerId);
+    alert('Time is out');
+    const diffTime = convertMs(0);
+    updateClockface(diffTime);
+    return;
+  }
+}
+
+function startTimer() {
+  timerId = setInterval(timer, 1000);
+}

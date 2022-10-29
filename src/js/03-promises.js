4de879d7a5e8dead62a378bee2b6c1c1a6abe652
Anatolii Fedorenko <anatolyfedorenko@gmail.com>
2022-10-29 12:25:58 +0300

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', promiseCounter);
+
+function promiseCounter(e) {
+  e.preventDefault();
+  const firstDelay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);
+  const amount = Number(form.elements.amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    const delay = firstDelay + i * step;
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) =>
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+  }
+  e.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
